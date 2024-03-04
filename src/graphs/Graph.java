1a069635ad29ddabf501809915dfe636a96078f3
francisco <franciscoricardo.dev@gmail.com>
2024-03-04 00:45:43 -0300

    Start the graphs algo
     Changes to be committed:
    	new file:   src/graphs/Graph.java
    	new file:   src/graphs/Main.java

---
+++ src/graphs/Graph.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Graph {
+
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getAdjList() {
+        return adjList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+
+        return false;
+    }
+
+	// WRITE ADDVERTEX METHOD HERE //
+	//                             //
+	//                             //
+	//                             //
+	//                             //
+	/////////////////////////////////
+
+}
---
+++ src/graphs/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph();
+        
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        
+        System.out.println("Graph:");
+        myGraph.printGraph();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Graph:
+            {A=[], B=[]}
+
+        */
+
+    }
+
+}
